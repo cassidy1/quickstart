a52aaa0f1e1aad866a4e3f627c45f7a9d064164b
Ryan Cassidy <ryancassidy0@gmail.com>
2020-12-02 14:39:27 -0800

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,19 +1,5 @@
 # Variables for AWS infrastructure module
 
-// TODO - use null defaults
-
-# Required
-variable "aws_access_key" {
-  type        = string
-  description = "AWS access key used to create infrastructure"
-}
-
-# Required
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS secret key used to create AWS infrastructure"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region used for all resources"
